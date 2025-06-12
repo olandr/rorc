b0c8983fc5aa196ca5a0ee2fd61bf94ba526b1d2
Simon Olander <41926603+olandr@users.noreply.github.com>
2025-06-12 13:28:42 +0200

    feat(pkg): adds fail, pass
    ---------
    
    by: Simon Olander (41926603+olandr@users.noreply.github.com)

--- pkg/matcher.go
+++ pkg/matcher.go
@@ -7,6 +7,8 @@ import (
 )
 
 var (
+	PASS_STYLE     = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("47"))
+	FAIL_STYLE     = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("160"))
 	REGEX_STYLE    = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("51"))
 	DEBUG_STYLE    = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("105"))
 	INFO_STYLE     = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("86"))
@@ -15,6 +17,8 @@ var (
 	FATAL_STYLE    = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("134"))
 	CRITICAL_STYLE = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("134"))
 
+	PASS_COMPILE, _     = regexp.Compile("(Pass|PASS|pass)")
+	FAIL_COMPILE, _     = regexp.Compile("(Fail|FAIL|fail)")
 	DEBUG_COMPILE, _    = regexp.Compile("(Debug|debug|DEBUG)")
 	INFO_COMPILE, _     = regexp.Compile("(Info|info|INFO)")
 	WARN_COMPILE, _     = regexp.Compile("(Warn|warn|WARN)")
@@ -26,6 +30,12 @@ var (
 func ApplyStyle(str string, re *regexp.Regexp) string {
 	if re != nil && re.MatchString(str) {
 		return REGEX_STYLE.Render(str)
+	}
+	if PASS_COMPILE.MatchString(str) {
+		return PASS_STYLE.Render(str)
+	}
+	if FAIL_COMPILE.MatchString(str) {
+		return FAIL_STYLE.Render(str)
 	}
 	if DEBUG_COMPILE.MatchString(str) {
 		return DEBUG_STYLE.Render(str)
